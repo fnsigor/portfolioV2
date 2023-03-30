ebe412f5880196a555fe754c4e7922fbca8649b2
Igor Fernandes <igorfernamdez@gmail.com>
2023-03-30 20:07:37 -0300

    centralize all texts

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -6,7 +6,9 @@ import { Carousel } from '../components/Carousel';
 import { useLoaderData } from "react-router-dom";
 import { Form } from '../components/Form';
 
-
+import { BsWhatsapp, BsTwitter, BsGithub, BsLinkedin } from 'react-icons/bs';
+import { MdEmail } from 'react-icons/md'
+import { SiFrontendmentor } from 'react-icons/si'
 
 
 export function Index() {
@@ -15,6 +17,7 @@ export function Index() {
 
 
 	const PageContent = styled.div`
+	
 
 		position: relative;
 
@@ -30,8 +33,13 @@ export function Index() {
 				margin-bottom: 2rem;
 			}
 
-			header, section{
+			section, header{
 				margin-bottom: 15rem ;
+			}
+
+
+			h4{
+				text-align: center;
 			}
 `
 
@@ -63,115 +71,100 @@ export function Index() {
 	`
 
 
-	const SectionAbout = styled.section`
-		
-	display: flex;
-	gap: 15rem;
+	const SectionAbout = styled.section`	
 
-	> div{
-		width: 50%;
+		p{
+			margin-inline: auto;
+			font-size: 2rem;
+			margin-bottom: 1rem;
+			width: 50%;
+		}
 
-	}
-
-	p{
-		font-size: 2rem;
-		margin-bottom: 1rem;
-	}
-
-	.stack-section{
-		display: flex;
-		flex-direction: column;
-	}
-
-	.stack-list{
-		display: flex;
-
-		gap: 3rem;
-
-		flex: 1;
-	}
-
-	.stack-list span{
-		font-size: 3rem;
-	}
-
-	.react-libs{
-		height: 300px;
-		width: 300px;
-	}
 
 	`
 
+	const SectionStack = styled.section`
+
+		.stack-list{
+			display: flex;
+			gap: 3rem;
+			justify-content: center;
+			flex: 1;
+		}
+
+		.stack-list span{
+			font-size: 3rem;
+		}
+
+		.react-libs{
+			height: 300px;
+			width: 300px;
+		}
+
+	`
 
 	const SectionProjects = styled.section`
-
-	h4{
-		text-align: center;
-	}
-	
 	`
 
 	const SectionContact = styled.section`
 	
-		h4{
-			text-align: center;
-		}
-	
-		.content{
-			display: flex;
-		}
-
-		.content > *{
-			flex: 1;
-		}
-
-		.formContainer, .contactsContainer{
-			display: flex;
-			justify-content:center;
-			align-items:center;
-		}
 
 		.contactsUl{
 			display: flex;
-			flex-direction:column;
-			gap: 4rem;
+			gap: 8rem;
+			justify-content: center;
 		}
 		
 		.contactsUl li{
-			position: relative;
+			height: 6rem;
+			width: auto;
 		}
 
-		.contactsUl li::after{
-			content: "";
-			position: absolute;
-			height: 2px;
+		svg{
+			height:100%;
 			width: 100%;
-			box-shadow: 0px 2px 2px ${props => props.theme.lightPurple};
-			background-color: transparent;
 		}
+	
 
 
-		.contactsUl span{
-			font-size: 1.8rem;
-			margin-bottom: .5rem;
-			display: block;
-		}
-
-		.contactsUl a {
-			font-size: 2.4rem;
-			color:  ${props => props.theme.paragraphColor};
-			transition: .3s;
-			font-weight: 200;
-			display: block;
-			
-		}
+		
+		
+		
 	`
 
 	const Footer = styled.footer`
 
+
+	padding-bottom: 1.5rem;
+	width: 100%;
+
 		h6{
-			font-size:1.6rem
+			font-size:1.4rem;
+			font-weight: 400;
+			/* background-color: red; */
+			line-height: 100%;
 		}
+
+		ul{
+			display: flex;
+			align-items: center;
+			width: 100%;
+			justify-content: center;
+		}
+
+		ul li:first-child{
+			margin-right: 5px;
+		}
+
+		li a{
+			font-size: 1.4rem;
+		}
+
+		ul li:not(ul li:first-child):not(ul li:last-child):after{
+			content: ",";
+			margin-right: 10px;
+		}
+
 	`
 
 	return (
@@ -185,33 +178,31 @@ export function Index() {
 				</Header>
 
 				<main>
-					<SectionAbout id='about'>
-						<div >
-							<h4 className="subtitle glow">Sobre</h4>
-							<p>Tive meu primeiro contato com programação em 2021, quando criei minhas primeiras aplicações Back-end, utilizando Java e MySQL</p>
-							<p> Por ser uma pessoa que gosta de usar a criatividade, não tive dúvidas no momento em que conheci o Front-end: era com a construção de interfaces utilizando código que eu queria trabalhar.</p>
-							<p>É uma área da programação que une as coisas que mais gosto: código e criatividade.</p>
-							<p>Em 2022 comecei essa jornada para me tornar um desenvolvedor Front-end. Atualmente estou dando meus primeiros passos com React e buscando um estágio na área.</p>
-						</div>
-
-
-						<div className="stack-section">
-							<h4 className="subtitle glow">Stack</h4>
-							<ul className="stack-list">
-								<li><span>Javascript</span></li>
-								<li><span>HTML</span></li>
-								<li><span>CSS</span></li>
-								<li>
-									<RadixPopUp
-										color="blue"
-										itemName="React"
-										popUpTitle="Ferramentas"
-										popUpDescription="Styled components, react router" />
-								</li>
-								<li><span>GIT</span></li>
-							</ul>
-						</div>
+					<SectionAbout >
+						<h4 className="subtitle glow">Sobre</h4>
+						<p>Tive meu primeiro contato com programação em 2021, quando criei minhas primeiras aplicações Back-end, utilizando Java e MySQL</p>
+						<p> Por ser uma pessoa que gosta de usar a criatividade, não tive dúvidas no momento em que conheci o Front-end: era com a construção de interfaces utilizando código que eu queria trabalhar.</p>
+						<p>É uma área da programação que une as coisas que mais gosto: código e criatividade.</p>
+						<p>Em 2022 comecei essa jornada para me tornar um desenvolvedor Front-end. Atualmente estou dando meus primeiros passos com React e buscando um estágio na área.</p>
 					</SectionAbout>
+
+					<SectionStack className="stack-section">
+						<h4 className="subtitle glow">Stack</h4>
+						<ul className="stack-list">
+							<li><span>Javascript</span></li>
+							<li><span>HTML</span></li>
+							<li><span>CSS</span></li>
+							<li>
+								<RadixPopUp
+									color="blue"
+									itemName="React"
+									popUpTitle="Ferramentas"
+									popUpDescription="Styled components, react router" />
+							</li>
+							<li><span>GIT</span></li>
+						</ul>
+					</SectionStack>
+
 
 					<SectionProjects>
 						<h4 className="subtitle glow">Projetos recentes</h4>
@@ -220,39 +211,36 @@ export function Index() {
 
 					<SectionContact>
 						<h4 className="subtitle glow">Contato e Redes Sociais</h4>
-						<div className="content">
-							<div className="formContainer">
-								<Form />
-							</div>
-							<div className="contactsContainer">
-								<ul className="contactsUl">
-									<li>
-										<span>Email: </span>
-										<a href="">igorfernamdez@gmail.com</a>
-									</li>
-									<li>
+						<address className="content">
+							<ul className="contactsUl">
+								<li>
+									<a href=""><MdEmail /></a>
+								</li>
+								<li>
+									<a href=""><BsWhatsapp /></a>
+								</li>
+								<li>
+									<a href=""><BsLinkedin /></a>
+								</li>
+								<li>
+									<a href=""><BsGithub /></a>
+								</li>
+								<li>
+									<a href=""><BsTwitter /></a>
+								</li>
+								<li>
+									<a href=""><SiFrontendmentor /></a>
+								</li>
+							</ul>
 
-										<span>Whatsapp:</span>
-										<a href="">48984489254</a>
-									</li>
-									<li>
-										<span>Linkedin: </span>
-										<a href="">igorfernamdez@gmail.com</a>
-									</li>
-									<li>
-										<span>Github: </span>
-										<a href="">fnsigor</a>
-									</li>
-								</ul>
-							</div>
-						</div>
+						</address>
 					</SectionContact>
 				</main>
 
 
 				<Footer>
-					<h6>Essa aplicação foi construida com as seguintes tecnologias:</h6>
 					<ul>
+						<li><h6>Ferramentas usadas para construção desse portfólio:</h6></li>
 						<li><a href="" target="_blank">React</a></li>
 						<li><a href="" target="_blank">Styled Components</a></li>
 						<li><a href="" target="_blank">Radix Ui</a></li>
