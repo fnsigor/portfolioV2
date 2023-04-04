ade303346aac769123ec78a01eacd7744f45eaca
Igor Fernandes <igorfernamdez@gmail.com>
2023-04-04 18:22:20 -0300

    set extension name at globalStyle import

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom/client'
 import { Index } from './pages/Index'
 import { createBrowserRouter, RouterProvider, } from "react-router-dom";
 import ErrorPage from './pages/ErrorPage';
-import { GlobalStyle } from './globalStyle';
 import { getSomeProjects } from './getSomeProjects';
 import { getAllProjects } from './getAllProjects';
 import { AllProjects } from './pages/AllProjects';
+import { GlobalStyle } from './globalStyle.js';
 
 
 const router = createBrowserRouter([
