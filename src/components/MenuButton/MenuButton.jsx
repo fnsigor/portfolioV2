588d177725f0c8349a8305e7d05b8c45045534e0
Igor Fernandes <igorfernamdez@gmail.com>
2023-04-12 14:47:24 -0300

    Desktop Finalizado :rocket:

---
+++ src/components/MenuButton/MenuButton.jsx
@@ -0,0 +1,47 @@
+import styled, { ThemeProvider } from 'styled-components';
+import colors from '../../GlobalStyles';
+import React from 'react';
+import * as Popover from '@radix-ui/react-popover';
+import { MixerHorizontalIcon, Cross2Icon } from '@radix-ui/react-icons';
+import { HashLink as Link } from 'react-router-hash-link';
+
+import './style.css'
+
+
+
+const MenuItems = styled.ul`
+
+
+`
+
+export function MenuButton() {
+
+    return (
+
+        <ThemeProvider theme={colors}>
+            <Popover.Root>
+                <Popover.Trigger asChild>
+                    <button className="IconButton" aria-label="Update dimensions" title='Menu'>
+                        <MixerHorizontalIcon />
+                    </button>
+                </Popover.Trigger>
+                <Popover.Portal>
+                    <Popover.Content className="PopoverContent" sideOffset={5}>
+                        <ul>
+                            <li><Link to="/#sobre">SOBRE</Link></li>
+                            <li><Link to="/#stack">STACK</Link></li>
+                            <li><Link to="/#projetos">PROJETOS RECENTES</Link></li>
+                            <li><Link to="/#contatos">CONTATO E REDES SOCIAIS</Link></li>
+                            <li><Link to="/projects">TODOS OS PROJETOS</Link></li>
+                        </ul>
+                        <Popover.Close className="PopoverClose" aria-label="Close">
+                            <Cross2Icon />
+                        </Popover.Close>
+                        <Popover.Arrow className="PopoverArrow" />
+                    </Popover.Content>
+                </Popover.Portal>
+            </Popover.Root>
+        </ThemeProvider>
+
+    )
+}
