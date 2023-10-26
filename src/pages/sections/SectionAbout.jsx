cd75b96860fa29f6dd715b672e203aea9f2e38e9
Igor Fernandes <igorfernamdez@gmail.com>
2023-10-26 14:44:38 -0300

    commit

--- src/pages/sections/SectionAbout.jsx
+++ src/pages/sections/SectionAbout.jsx
@@ -36,7 +36,7 @@ export function SectionAbout() {
     return (
         <Content id='sobre'>
             <h4 className="subtitle glow">Sobre</h4>
-            <p>Tive meu primeiro contato com programação em <span>2021</span>, quando criei minhas primeiras aplicações Back-end, utilizando Java e MySQL</p>
+            <p>Tive meu primeiro contato com programação em <span>2021</span>, quando criei minhas primeiras aplicações Back-end, utilizando Java e MySQL.</p>
             <p> Por ser uma pessoa que gosta de usar a criatividade, não tive dúvidas no momento em que conheci o Front-end: era com a construção de interfaces utilizando código que eu queria trabalhar.</p>
             <p>É uma área da programação que une as coisas que mais gosto: código e criatividade.</p>
             <p>Iniciei no mercado de trabalho como dev Front-end, porém assumi responsabilidades no Back-end e quando vi, já tinha me tornado um Fullstack.</p>
